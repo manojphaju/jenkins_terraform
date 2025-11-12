858f541596cf4374da30eb333ac5b06cf692c2c5
Manoj Phaju <phajumanoj@gmail.com>
2025-11-12 15:51:37 +0545

    ami image ap-southeast-2 updated in main

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 provider "aws" {
-    region = "ap-southeast-2"  
+  region = "ap-southeast-2"
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-0b8d527345fdace59" # us-west-2
+  ami           = "ami-0d5d9d301c853a04a"  # Amazon Linux 2 in Sydney
   instance_type = "t2.micro"
+
   tags = {
-      Name = "TF-Instance"
+    Name = "TF-Instance"
   }
 }
